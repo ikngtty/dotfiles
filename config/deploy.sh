355938aefe89407e86337528191e38251350e5f6
ikngtty <ikngtty@gmail.com>
2018-12-01 05:33:11 +0900

    Add a feature to specify deploy file's pattern.

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -eu -o pipefail
 
+#####
+# Arguments
+# $1: A pattern for deploy file's path. Default value is "".
+#####
+pattern_deploy=""
+[ $# -eq 1 ] && pattern_deploy=$1
+
 # Get project directory's absolute path.
 # NOTE: Should do before changing the working directory because
 # `$0` returns a relative path.
@@ -17,17 +24,18 @@ cd "$dir_project"
 cd "$dir_here"
 ./check_deploy_status.sh    |
   tail -n +2                | # Remove the header
-    while IFS="$(printf "\t")" read deploy_from deploy_to deploy_status
-    do
-      case "$deploy_status" in
-        "UNDEPLOYED")
-          printf "Deploying $(basename "$deploy_from") ... "
-          ln -s "$deploy_from" "$(dirname "$deploy_to")"
-          echo "Done!"
-          ;;
-        "CONFLICT")
-          echo "[WARNING] Failed to deploy $(basename "$deploy_from")"\
-            "because it conflicts. Please check and resolve it."
-          ;;
-      esac
-    done
+    grep "$pattern_deploy"  |
+      while IFS="$(printf "\t")" read deploy_from deploy_to deploy_status
+      do
+        case "$deploy_status" in
+          "UNDEPLOYED")
+            printf "Deploying $(basename "$deploy_from") ... "
+            ln -s "$deploy_from" "$(dirname "$deploy_to")"
+            echo "Done!"
+            ;;
+          "CONFLICT")
+            echo "[WARNING] Failed to deploy $(basename "$deploy_from")"\
+              "because it conflicts. Please check and resolve it."
+            ;;
+        esac
+      done
