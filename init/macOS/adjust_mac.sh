b8089f86c0edc2f784e40cab5b81cfae3e161f9e
ikngtty <ikngtty@gmail.com>
2018-12-03 00:33:28 +0900

    Improve output messages of `adjust_mac.sh`.

--- init/macOS/adjust_mac.sh
+++ init/macOS/adjust_mac.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-# WARNING: Need to reboot to reflect changes.
+# Get project directory's absolute path.
+# NOTE: Should do before changing the working directory because
+# `$0` returns a relative path.
+my_file_name="$(basename "$0")"
+cd "$(dirname "$0")"
+# dir_here=$(pwd)
+cd ../../
+dir_project=$(pwd)
 
+# Read the common part.
+cd "$dir_project"
+. ./__common.sh "$dir_project"
+
+here_log() {
+  log "$my_file_name" "$1"
+}
+
+here_log "Change preferences of Desktop, Finder, ScreenCapture, etc."
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
@@ -15,5 +31,10 @@ defaults write com.apple.screencapture disable-shadow -bool true
 # defaults write com.apple.screencapture location ~/Pictures
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
+here_log "Change directory's visibility."
 chflags nohidden ~/Library
 sudo chflags nohidden /Volumes
+
+printf "\e[32m"
+printf "Yeah! $my_file_name complete! Please reboot to reflect settings."
+printf "\e[m\n"
