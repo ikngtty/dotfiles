bc09c4d1eda6eb56d460d7b184486c4b6ebae65a
ikngtty <ikngtty@gmail.com>
2019-05-03 02:20:55 +0900

    feat: add a script to bundle conda packages.

---
+++ config/original_files/home/Configurations/bundle-conda-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -Ceu -o pipefail
+
+# Check Requirements.
+if ! which conda >/dev/null 2>&1; then
+  printf "\e[31m"                   # Red
+  printf "Cannot run! Please install "
+  printf "\e[1mconda\e[m"              # Bold and reset
+  printf "\e[31m"                   # Red again
+  printf "!"
+  printf "\e[m\n"                   # Reset
+  exit 10
+fi
+
+install_message() {
+  package_name=$1
+  printf "\e[36m"         # Magenta
+  printf "Install $package_name..."
+  printf "\e[m\n"         # Reset
+}
+
+install_message pylama
+conda install -c conda-forge pylama
+
+printf "\e[32m"     # Green
+printf "Done!"
+printf "\e[m\n"     # Reset
