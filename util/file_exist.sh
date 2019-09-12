fe009750f63bd0c93d7043c3d261e907fd34c7e3
ikngtty <ikngtty@gmail.com>
2019-09-12 21:48:36 +0900

    :green_heart: improve comments
    Not only my MBP but also my MBA meets that spec.

--- util/file_exist.sh
+++ util/file_exist.sh
@@ -4,8 +4,8 @@ if set -o | grep pipefail >/dev/null 2>&1; then
   set -o pipefail
 fi
 
-# NOTE: My MacBook Pro (Mojabe) says "there is no file in the specified path"
+# NOTE: My Mac (Mojabe) says "there is no file in the specified path"
 # when the specified path is a symlink and the linked file is lost.
-# But if I try to create a new file in the specified path, the MBP says
-# "Error! the specified file exists!". Troublesome!
+# But if I try to create a new file in the specified path, my Mac says
+# "Error! the specified file exists!". It is undesirable.
 [ -e "$1" ] || readlink "$1" >/dev/null 2>&1
