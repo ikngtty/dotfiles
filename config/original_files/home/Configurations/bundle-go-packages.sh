a030c82e54f3a612cf833aa859543feae9dd3461
ikngtty <ikngtty@gmail.com>
2019-11-02 21:38:16 +0900

    (bundle-go) use module mode
    The bundle-go script gets more speedy because it uses cache.

--- config/original_files/home/Configurations/bundle-go-packages.sh
+++ config/original_files/home/Configurations/bundle-go-packages.sh
@@ -21,49 +21,49 @@ if ! which go >/dev/null 2>&1; then
 fi
 
 get_with_info() {
-  package_url=$1
+  module_mode=$1
+  package_url=$2
   package_name=$(basename "$package_url")
   printf "\e[36m"         # Magenta
   printf "Get $package_name..."
   printf "\e[m\n"         # Reset
-  go get -u "$package_url"
+  GO111MODULE="$module_mode" go get -u "$package_url"
 }
 
 # Install.
-# TODO: Find a tool to manage dependency without a vendor directory.
 ## For Atom's go-plus package.
-get_with_info github.com/fatih/gomodifytags
-get_with_info github.com/mdempsky/gocode
-get_with_info github.com/ramya-rao-a/go-outline
-get_with_info github.com/tpng/gopkgs
-get_with_info github.com/zmb3/goaddimport
-get_with_info github.com/zmb3/gogetdoc
-get_with_info golang.org/x/tools/cmd/gorename
-get_with_info golang.org/x/tools/cmd/guru
+get_with_info on github.com/fatih/gomodifytags
+get_with_info on github.com/mdempsky/gocode
+get_with_info on github.com/ramya-rao-a/go-outline
+get_with_info on github.com/tpng/gopkgs
+get_with_info on github.com/zmb3/goaddimport
+get_with_info on github.com/zmb3/gogetdoc
+get_with_info on golang.org/x/tools/cmd/gorename
+get_with_info on golang.org/x/tools/cmd/guru
 ### Formatters. (Choose one.)
-get_with_info golang.org/x/tools/cmd/goimports
-get_with_info github.com/sqs/goreturns
+get_with_info on golang.org/x/tools/cmd/goimports
+get_with_info on github.com/sqs/goreturns
 ### Linters. (Choose one.)
-get_with_info github.com/alecthomas/gometalinter
-get_with_info github.com/golangci/golangci-lint/cmd/golangci-lint
-get_with_info github.com/mgechev/revive
+get_with_info off github.com/alecthomas/gometalinter # on -> mysterious error
+get_with_info on github.com/golangci/golangci-lint/cmd/golangci-lint
+get_with_info on github.com/mgechev/revive
 ## Navigators. (Choose one.)
-get_with_info github.com/rogpeppe/godef
+get_with_info on github.com/rogpeppe/godef
 
 ## For Atom's go-debug package.
-get_with_info github.com/derekparker/delve/cmd/dlv
+get_with_info off github.com/derekparker/delve/cmd/dlv # on -> mysterious error
 
 ## Gore (REPL).
-get_with_info github.com/motemen/gore/cmd/gore
+get_with_info on github.com/motemen/gore/cmd/gore
 ### Additional features.
 # get_with_info github.com/mdempsky/gocode
-get_with_info github.com/k0kubun/pp
-get_with_info golang.org/x/tools/cmd/godoc
+get_with_info on github.com/k0kubun/pp
+get_with_info on golang.org/x/tools/cmd/godoc
 
 ## Others.
-get_with_info bitbucket.org/liamstask/goose/cmd/goose
-get_with_info github.com/BurntSushi/toml/cmd/tomlv
-get_with_info golang.org/x/tools/cmd/stringer
+get_with_info on bitbucket.org/liamstask/goose/cmd/goose
+get_with_info on github.com/BurntSushi/toml/cmd/tomlv
+get_with_info on golang.org/x/tools/cmd/stringer
 
 printf "\e[32m"     # Green
 printf "Done!"
